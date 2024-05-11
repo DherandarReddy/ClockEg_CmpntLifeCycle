65ee3ba1849e041b307727cbec13ff837adeed97
Dherandar08102003 <ydreddy0810@gmail.com>
2024-05-11 09:55:22 +0530

    Digital Clock

--- src/App.js
+++ src/App.js
@@ -1,39 +1,7 @@
-import {Component} from 'react'
-import Clock from './components/Clock'
+import DigitalTimer from './components/DigitalTimer'
 
 import './App.css'
 
-class App extends Component {
-  state = {showClock: false}
-
-  onToggleClock = () => {
-    this.setState(prevState => ({showClock: !prevState.showClock}))
-
-    /*
-    this.setState(prevState => {
-      const {showClock} = prevState
-      return {
-        showClock: !showClock,
-      }
-    })
-    */
-  }
-
-  render() {
-    const {showClock} = this.state
-    return (
-      <div className="app-container">
-        <button
-          type="button"
-          className="toggle-button"
-          onClick={this.onToggleClock}
-        >
-          {showClock ? 'Hide Clock' : 'Show Clock'}
-        </button>
-        {showClock && <Clock />}
-      </div>
-    )
-  }
-}
+const App = () => <DigitalTimer />
 
 export default App
